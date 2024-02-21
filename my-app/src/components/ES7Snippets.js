df972ed5ad731f768e198c44075681cf964ac3a3
Kayleigh Taylor <ktay0557@gmail.com>
2024-02-21 14:06:24 +0000

    Learning ES7 Snippets for React

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,68 @@
+/**
+ * uses rfce to create a React Functional Component
+ */
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+/**
+ * Uses rafce to create a
+ * React Arrow Function Component
+ */
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+/**
+ * Uses rce to create a React Class Component
+ */
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+/**
+ * Uses rconst to add a default constructor
+ * for a class that contains props as arguments
+ */
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+/**
+ * Uses clg to console.log
+ */
+// console.log(first)
+
+
+/**
+ * Uses clo to console.log with objects
+ */
+// console.log('first', first)
