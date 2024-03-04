9d641cc5c186bba3c7ccc41dfc1f3060ee3a6136
Kayleigh Taylor <ktay0557@gmail.com>
2024-03-04 12:16:18 +0000

    Working through the course content for CI to understand React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,51 @@
-import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
-import NavBarSimple from "./components/NavBarSimple";
+// import './App.css';
+// import Sidebar from "./components/Sidebar";
+// import NavBarSimple from "./components/NavBarSimple";
+// import EventsFunctional from './components/EventsFunctional';
+// import EventsClass from './components/EventsClass';
+
+// function App() {
+//   return <div className="App">
+//       <EventsFunctional />
+//       <EventsClass />
+//     </div>;
+// }
+
+// export default App;
+
+// import "./App.css";
+// import EventBinding from "./components/EventBinding";
+
+// function App() {
+//   return <div className="App">
+//       <EventBinding />
+//   </div>;
+// }
+
+// export default App;
+
+// import "./App.css";
+// import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+// import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <ConditionalRenderingClass />
+//       <ConditionalRenderingFunctional connected={false} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import "./App.css";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
   return (
-    <div className={css.App}>
-      {/* Add your components here */}
-      <NavBarSimple />
-      <Sidebar />
+    <div className="App">
+      <NestingComponents />
     </div>
   );
 }
